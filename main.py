5918c1bb2dd8cd6ff43f32b010612fa5ea010ee1
Sunny <sunnyghangas098@gmail.com>
2025-02-13 22:53:40 +0530

    Data Validation completed

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from claimprediction.exception.exception import CarInsuranceException
 from claimprediction.logging.logger import logging
 from claimprediction.components.data_ingestion import DataIngestion
-from claimprediction.entity.artifact_entity import DataIngestionArtifact
-from claimprediction.entity.config_entity import DataIngestionConfig
+from claimprediction.components.data_validation import DataValidation
+from claimprediction.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
+from claimprediction.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from claimprediction.entity.config_entity import TrainingPipelineConfig
 import sys
 
 if __name__=="__main__":
     try:
+        ## Data Ingestion
+
         training_pipeline_config = TrainingPipelineConfig()
         data_ingestion_config = DataIngestionConfig(training_pipeline_config)
         data_ingestion = DataIngestion(data_ingestion_config)
@@ -15,6 +18,14 @@ if __name__=="__main__":
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Ingestion completed")
         print(data_ingestion_artifact)
+
+        ## Data Validation
+
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info("INitiate data validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data validation completed")
 
     except Exception as e:
         raise CarInsuranceException(e)
